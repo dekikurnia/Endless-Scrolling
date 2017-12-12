77ba20a1b849da5c7049be0b684216db80eacc59
dekikurnia <dekikurnia@gmail.com>
2017-12-12 12:18:47 +0700

    Upload pertama

---
+++ app/src/main/java/com/dekikurnia/scrolling/MainActivity.java
@@ -0,0 +1,109 @@
+package com.dekikurnia.scrolling;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<Contact> contacts;
+    private ContactAdapter contactAdapter;
+    private Random random;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        contacts = new ArrayList<>();
+        random = new Random();
+
+        //set dummy data
+        for (int i = 0; i < 10; i++) {
+            Contact contact = new Contact();
+            contact.setPhone(phoneNumberGenerating());
+            contact.setEmail("DevExchanges" + i + "@gmail.com");
+            contacts.add(contact);
+        }
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        contactAdapter = new ContactAdapter(recyclerView, contacts, this);
+        recyclerView.setAdapter(contactAdapter);
+
+        contactAdapter.setOnLoadMoreListener(new OnLoadMoreListener() {
+            @Override
+            public void onLoadMore() {
+                if (contacts.size() <= 20) {
+                    contacts.add(null);
+                    contactAdapter.notifyItemInserted(contacts.size() - 1);
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            contacts.remove(contacts.size() - 1);
+                            contactAdapter.notifyItemRemoved(contacts.size());
+
+                            //Generating more data
+                            int index = contacts.size();
+                            int end = index + 10;
+                            for (int i = index; i < end; i++) {
+                                Contact contact = new Contact();
+                                contact.setPhone(phoneNumberGenerating());
+                                contact.setEmail("DevExchanges" + i + "@gmail.com");
+                                contacts.add(contact);
+                            }
+                            contactAdapter.notifyDataSetChanged();
+                            contactAdapter.setLoaded();
+                        }
+                    }, 3000);
+                } else {
+                    Toast.makeText(MainActivity.this, "Loading data completed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private String phoneNumberGenerating() {
+        int low = 100000000;
+        int high = 999999999;
+        int randomNumber = random.nextInt(high - low) + low;
+
+        return "0" + randomNumber;
+    }
+}
---
+++ app/src/main/java/com/dekikurnia/scrolling/OnLoadMoreListener.java
@@ -0,0 +1,9 @@
+package com.dekikurnia.scrolling;
+
+/**
+ * Created by server02 on 12/12/2017.
+ */
+
+public interface OnLoadMoreListener {
+    void onLoadMore();
+}
